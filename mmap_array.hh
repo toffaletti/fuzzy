5c17eb15c571866750a3f62e1acb2a4708f49a99
Jason Toffaletti <toffaleti@gmail.com>
2013-01-15 13:33:30 -0800

    better exceptions

--- mmap_array.hh
+++ mmap_array.hh
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 
 #include <string>
+#include <sstream>
 #include <stdexcept>
 
 template <class T>
@@ -15,34 +16,44 @@ private:
     int _fd = -1;
     T *_map = (T *)MAP_FAILED;
 public:
+    class error : public std::runtime_error {
+        public:
+            error(const std::ostringstream &ss) : std::runtime_error(ss.str()) {}
+    };
+public:
     static_assert(4096 % sizeof(T) == 0, "sizeof(T) not page aligned");
     // non-copyable
     mmap_array(const mmap_array&) = delete;
     mmap_array &operator=(const mmap_array &) = delete;
 
-    mmap_array() {}
-
     mmap_array(size_t size) : _size(size) {
         _map = (T *)::mmap(NULL, size * sizeof(T), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
         if (_map == MAP_FAILED) {
-            throw std::runtime_error(std::string("could not mmap ") + 
-                    sys_errlist[errno]);
+            std::ostringstream ss;
+            ss << "mmap failed " << sys_errlist[errno];
+            throw error(ss);
         }
     }
 
     mmap_array(const char *filename, size_t size, int flags=0700) : _size(size) {
         _fd = ::open(filename, O_CREAT | O_RDWR, flags);
         if (_fd == -1) {
-            throw std::runtime_error(std::string("could not open ") + filename);
+            std::ostringstream ss;
+            ss << "opening " << filename << " failed " << sys_errlist[errno];
+            throw error(ss);
         }
         int status = ::posix_fallocate(_fd, 0, size * sizeof(T));
         if (status != 0) {
-            throw std::runtime_error(std::string("could not allocate ") + filename);
+            std::ostringstream ss;
+            ss << "fallocate " << filename << " failed " << sys_errlist[errno];
+            throw error(ss);
         }
         _map = (T *)::mmap(NULL, size * sizeof(T), PROT_READ | PROT_WRITE, MAP_SHARED, _fd, 0);
         if (_map == MAP_FAILED) {
             close();
-            throw std::runtime_error(std::string("could not mmap ") + filename);
+            std::ostringstream ss;
+            ss << "mmap " << filename << " failed " << sys_errlist[errno];
+            throw error(ss);
         }
     }
 
